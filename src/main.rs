7e90ed9a426cae71df5c7c7403843c5a3a04c445
Sourcedev <sourcedev@posteo.org>
2021-11-26 16:17:09 +0100

    mqtt lib changed

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
-extern crate i2cdev;
-
-use i2cdev::linux::LinuxI2CDevice;
+use linux_embedded_hal::{Delay, I2cdev};
 
 use std::thread;
 use std::time::Duration;
-//use std::io::prelude::*;
 
 //use gpio::GpioOut;
-use rumqtt::{MqttClient, MqttOptions, QoS};
+use rumqttc::{Client, MqttOptions, QoS};
 
 // ------------------------------------------------------------------------------
 // HTU21D stuff
 // ------------------------------------------------------------------------------
-mod libs;
-use crate::libs::bmp280::Bmp280Builder;
-use crate::libs::veml6070::VEML6070;
+//mod libs;
+use bme280::BME280;
 
 // ------------------------------------------------------------------------------
 // Main loop
@@ -24,44 +20,32 @@ fn main() {
     println!("| Interface test with Rust :) |");
     println!("-------------------------------");
 
-    let mqtt_options = MqttOptions::new("test-pubsub1", "192.168.2.2", 1883);
-    let (mut mqtt_client, notifications) = MqttClient::start(mqtt_options).unwrap();
+    let mut mqtt_options = MqttOptions::new("pi-oben", "192.168.2.2", 1883);
+    mqtt_options.set_keep_alive(Duration::from_secs(5));
 
+    let (mut mqtt_client, mut connection) = Client::new(mqtt_options, 10);
     mqtt_client.subscribe("sensor/oben", QoS::AtLeastOnce).unwrap();
-        
-    //    read_mcp();
-    let mut htu21_sen = libs::htu21::HTU21Sensor{ Temperatur: 0.0, Humidity: 0.0, Dev: LinuxI2CDevice::new("/dev/i2c-1", libs::htu21::SLAVE_ADDR_PRIMARY).unwrap() };
-    let mut uvSensor: VEML6070 = libs::veml6070::UVSensor::new("/dev/i2c-1");
-    
-    let mut bmp280 = Bmp280Builder::new()
-        .path("/dev/i2c-1".to_string())
-        .address(0x76)
-        .build()
-        .expect("Failed to build device");
 
-    bmp280.zero().expect("failed to zero");
     
+    let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
+    let mut bme280 = BME280::new_primary(i2c_bus, Delay);
+    bme280.init().unwrap();
+
     thread::spawn(move || loop {
-        htu21_sen.Process();
-        let uv = uvSensor.ReadUV();
-        let bmp_temp = bmp280.read_temperature().unwrap();
-        let bmp_pres = bmp280.read_pressure().unwrap()/100.0;
-        let bmp_alt = bmp280.read_altitude().unwrap();
-        
-        let temp = (htu21_sen.Temperatur + bmp_temp) / 2.0;
-        
-        println!("{:?} hPa", bmp280.read_pressure().unwrap());
-        println!("{:?} m", bmp280.read_altitude().unwrap());
-        println!("{:?} c", temp);
+        let meas = bme280.measure().unwrap();
+        let pres = meas.pressure / 100.0;
 
-        let payload = format!("{{\"Oben\": {{\"Temperature\":{}, \"Humidity\":{}, \"Pressure\":{}, \"Altitude\":{}, \"UV\":{}}}}}", temp, htu21_sen.Humidity, bmp_pres, bmp_alt, uv);
+        println!("Bmp280 pressure: {:?} kPa", pres);
+        println!("Bmp280 humidity: {:?} %", meas.humidity);
+        println!("Bmp280 temperature: {:?} c", meas.temperature);
+
+        let payload = format!("{{\"Oben\": {{\"Temperature\":{}, \"Humidity\":{}, \"Pressure\":{} }}}}", meas.temperature, meas.humidity, pres);
         mqtt_client.publish("sensor/oben", QoS::AtLeastOnce, false, payload).unwrap();
-        
-        // at least sleep for 5 seconds
-        thread::sleep(Duration::from_millis(60000));
-    });
 
-    for notification in notifications {
+        // at least sleep for 5 seconds
+        thread::sleep(Duration::from_millis(360000));
+    });
+    for (i, notification) in connection.iter().enumerate() {
         println!("{:?}", notification)
     }
 }
