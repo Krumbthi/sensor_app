e6d673f16d1c5d76f8c8862c2595269ff6d8e49a
thilo <t.krumbholz@bechstein-digital.com>
2021-12-21 11:59:39 +0100

    update

--- src/libs/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod htu21;
-pub mod veml6070;
-pub mod bmp280;
--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,33 @@ use std::{thread, process};
 use std::time::Duration;
 
 use rumqttc::{MqttOptions, Client, QoS, Event, Packet};
+use log::{info, debug, error};
+use env_logger::Env;
+
 
 // ------------------------------------------------------------------------------
 // HTU21D stuff
 // ------------------------------------------------------------------------------
 //mod libs;
-use bme280::BME280;
+use create::bme280::BME280;
 
 // ------------------------------------------------------------------------------
 // Main loop
 // ------------------------------------------------------------------------------
 fn main() {
-    println!("-------------------------------");
-    println!("| Interface test with Rust :) |");
-    println!("-------------------------------");
+    let env = Env::filter_or(Env::default(), "APP_LOG_LEVEL", "debug")
+        .write_style_or("APP_LOG_STYLE", "always");    
+    env_logger::init_from_env(env);
+
+    info!("-------------------------------");
+    info!("| Interface test with Rust :) |");
+    info!("-------------------------------");
 
     let mut mqtt_options = MqttOptions::new("pi-oben", "192.168.2.2", 1883);
     mqtt_options.set_keep_alive(Duration::from_secs(5));
 
     let (mut mqtt_client, mut connection) = Client::new(mqtt_options, 10);
-    mqtt_client.subscribe("sensor/oben", QoS::AtLeastOnce).unwrap();
+    mqtt_client.subscribe("sensor/rpi0", QoS::AtLeastOnce).unwrap();
 
     
     let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
@@ -34,12 +41,12 @@ fn main() {
         let meas = bme280.measure().unwrap();
         let pres = meas.pressure / 100.0;
 
-        println!("Bmp280 pressure: {:?} kPa", pres);
-        println!("Bmp280 humidity: {:?} %", meas.humidity);
-        println!("Bmp280 temperature: {:?} c", meas.temperature);
+        debug!("Bmp280 pressure: {:?} kPa", pres);
+        debug!("Bmp280 humidity: {:?} %", meas.humidity);
+        debug!("Bmp280 temperature: {:?} c", meas.temperature);
 
-        let payload = format!("{{\"Oben\": {{\"Temperature\":{}, \"Humidity\":{}, \"Pressure\":{} }}}}", meas.temperature, meas.humidity, pres);
-        mqtt_client.publish("sensor/oben", QoS::AtLeastOnce, false, payload).unwrap();
+        let payload = format!("{{\"Rpi\": {{\"Temperature\":{}, \"Humidity\":{}, \"Pressure\":{} }}}}", meas.temperature, meas.humidity, pres);
+        mqtt_client.publish("sensor/rpi0", QoS::AtLeastOnce, false, payload).unwrap();
 
         // at least sleep for 5 seconds
         thread::sleep(Duration::from_millis(360000));
@@ -51,7 +58,7 @@ fn main() {
                 //debug!("Incomming: {:?}", i);
                 match i {
                     Packet::Publish(p) => {
-                        println!("Payload: {:?}", p.payload);
+                        info!("Payload: {:?}", p.payload);
                         //if p.payload.to_vec() == "exit" {
                         if p.payload.slice(0..4) == "exit" {
                             process::exit(0);
@@ -61,7 +68,7 @@ fn main() {
                 }
             }
             //Ok(Event::Outgoing(o)) => debug!("Outgoing: {:?}", o),
-            Err(e) => println!("Error = {:?}", e),
+            Err(e) => error!("Error = {:?}", e),
             _ => (),
         }
     }
